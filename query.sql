218dca1cc80090b7b94a4e6f3799fa04ea849061
Md Alamin <tanviralamin221@gmail.com>
2026-06-26 01:52:56 +0600

    Update query.sql

--- query.sql
+++ query.sql
@@ -138,7 +138,7 @@ where tournament_category = 'Champions League' and match_status = 'Available'
 
 ---Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
 select user_id, full_name, email from users
-where full_name ilike 'Tanvir%' or full_name like '%haque%'
+where full_name ilike 'Tanvir%' or full_name ilike '%haque%'
 
 --Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
 select booking_id, user_id, match_id, coalesce(payment_status, 'Action Required') as  systematic_status from bookings
